5c0ed11d89e5c932146891a58fb0ebc45145939e
Sergii Rodymenko <sergii.rodymenko@gmail.com>
2020-06-29 19:18:57 +0300

    sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,19 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'lowerAsc') {
+export function sortStrings(arr, param = 'asc') {
+  let sortedArray = arr.slice();
+  for (let i = 0; i < sortedArray.length; i++) {
+    for (let j = 0; j < sortedArray.length; j++) {
+      if (param === 'asc' && sortedArray[i].localeCompare(sortedArray[j], [], {'caseFirst':  'upper'}) < 0 ||
+        param === 'desc' && sortedArray[i].localeCompare(sortedArray[j], [], {'caseFirst':  'upper'}) > 0)
+      {
+        let max = sortedArray[i];
+        sortedArray[i] = sortedArray[j];
+        sortedArray[j] = max;
+      }
+    }
+  };
 
+  return sortedArray;
 }
