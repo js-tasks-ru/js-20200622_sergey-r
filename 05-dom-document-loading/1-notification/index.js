95f5d79cdc50116fa28e33958597af873f4b34ee
Sergii Rodymenko <sergii.rodymenko@gmail.com>
2020-07-12 12:21:15 +0300

    notification + sortable table

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
+  static activeNotification;
 
+  constructor(message, {
+    duration = 2000,
+    type = 'success',
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.removeActiveNotification();
+    this.render();
+  }
+
+  get template() {
+    return `<div class="notification ${this.type}" style="--value:${Math.round(this.duration / 100)}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header"> ${this.type}</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+    </div>`;
+  }
+
+  removeActiveNotification() {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+  }
+
+  render() {
+    const element = document.createElement('div')
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  show(elem = document.body) {
+    elem.append(this.element);
+    NotificationMessage.activeNotification = this;
+    setTimeout(() => {
+      this.destroy();
+    },
+    this.duration
+    );
+  }
+
+  remove()
+  {
+    this.element.remove();
+  }
+
+  destroy()
+  {
+    this.remove();
+    NotificationMessage.activeNotification = null;
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,4 +1,110 @@
 export default class SortableTable {
+  element;
+  subElements = {};
+  header = [];
+  data = [];
 
+  constructor(header, data) {
+    this.header = header;
+    this.data = data.data;
+
+    this.render();
+  }
+
+  render() {
+    const element = document.createElement('div')
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+
+    this.subElements = this.getSubElements(element);
+  }
+
+  get headerTpl() {
+    return `<div data-element="header" class="sortable-table__header sortable-table__row">
+      ${this.headerRow}
+    </div>`;
+  }
+
+  get headerRow() {
+    return this.header.map(item =>
+      `<div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}"><span>${item.title}</span>
+        ${this.sortingArrowTpl}
+       </div>`
+    ).join('');
+  }
+
+  get sortingArrowTpl() {
+    return `
+        <span class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>`;
+  }
+
+  get bodyTpl() {
+    return `<div data-element="body" class="sortable-table__body">
+      ${this.bodyRows}
+    </div>`;
+  }
+
+  get bodyRows() {
+    return this.data.map(rowItem => {
+      const cells = this.header.map(cellItem => {
+        return cellItem.hasOwnProperty('template') ?
+          `${cellItem.template(rowItem.images)}` :
+          `<div class="sortable-table__cell" >${rowItem[cellItem.id]}</div>`;
+      }).join('');
+
+      return `<a href="/products/${rowItem.id}" class="sortable-table__row">
+        ${cells}
+      </a>`;
+    }).join('');
+  }
+
+  get template() {
+    return `<div class="sortable-table">
+      ${this.headerTpl}
+      ${this.bodyTpl}
+    </div>`;
+  }
+
+  sort(column, order) {
+    const sortType = this.header.find(item => item.id === column).sortType;
+    const sign = order === 'asc' ? 1 : -1;
+
+    const allColumns = document.querySelectorAll('.sortable-table__cell[data-id]');
+    const currentColumn = document.querySelector(`.sortable-table__cell[data-id="${column}"]`);
+
+    allColumns.forEach(column => {
+      column.dataset.order = '';
+    });
+    currentColumn.dataset.order = order;
+
+    this.data.sort((item1, item2) => {
+      switch (sortType) {
+      case 'number':
+        return sign * (item1[column] - item2[column]);
+      case 'string':
+        return sign * item1[column].localeCompare(item2[column], 'ru');
+      }
+    });
+
+    this.subElements.body.innerHTML = this.bodyTpl;
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+      return accum;
+    });
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
 
